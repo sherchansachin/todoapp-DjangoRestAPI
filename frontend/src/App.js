e990011abc9ca2407cc4ef835019d080bf69d6ae
sherchansachin <sachinsherchan3@gmail.com>
2021-07-09 21:54:01 +0545

    card component and styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import UpperTodo from './components/UpperTodo'
 import AddTask from './components/AddTask';
+import TodoCards from './components/TodoCards';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <UpperTodo/>
       <AddTask/>
+      <TodoCards/>
     </>
   );
 }
