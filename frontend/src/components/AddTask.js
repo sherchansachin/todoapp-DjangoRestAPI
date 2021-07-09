202eb966ec86ebcbccc31ba53d5e6828af7137b4
sherchansachin <sachinsherchan3@gmail.com>
2021-07-09 15:25:10 +0545

    added modal form

--- frontend/src/components/AddTask.js
+++ frontend/src/components/AddTask.js
@@ -1,14 +1,49 @@
-import React from 'react'
-import { Container} from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Container } from 'react-bootstrap';
+import { Modal, Button } from "react-bootstrap";
 
 const AddTask = () => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return ( 
-        <Container>
-            <div className='py-5 text-center tasks'>
-                <h3>What do I need to do today?</h3>
-                <button className='btn btn-primary mt-4'>Add a New Task</button>
-            </div>
-        </Container>
+        <>
+            <Container>
+                <div className='py-5 text-center tasks'>
+                    <h3>What do I need to do today?</h3>
+                    <button className='btn btn-primary mt-4' onClick={handleShow}>Add a New Task</button>
+                </div>
+            </Container>
+
+            <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Add a New Todo</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <form>
+                        <div className = 'form-group'>
+                            <label>To do Title</label>
+                            <input type='text' className='form-control'/>
+                        </div>
+
+                        <div className = 'form-group'>
+                            <label>Description</label>
+                            <textarea rows='4' className='form-control'></textarea>
+                        </div>
+                    </form>
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="primary" onClick={handleClose}>
+                    Add todo
+                </Button>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+            </Modal.Footer>
+            </Modal>
+        </>
     )
 }
 
